1a10296e46888f76ca762075546fd8bedac25527
taw <info@lucidpolygon.com>
2026-02-19 20:03:31 +0000

    Upgraded from tailwind 3 to 4

--- content/partials/header.php
+++ content/partials/header.php
@@ -25,7 +25,7 @@
                     </div>
                     <div class="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-2/3 md:flex-row md:py-0">
                         <a href="#" class="w-full px-6 py-2 mr-0 text-gray-700 md:px-3 md:mr-2 lg:mr-3 md:w-auto">Sign In</a>
-                        <a href="#_" class="inline-flex items-center w-full px-5 px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800">Sign Up</a>
+                        <a href="#_" class="inline-flex items-center w-full px-5 px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-hidden md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800">Sign Up</a>
                     </div>
                 </div>
             </div>
